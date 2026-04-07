d78e6334a9d657ee90e361f7418a8f6132f91ba7
Yasu Sakakibra <ysakakibara@gmail.com>
2026-04-07 08:25:22 -0700

    chore: remove Qt6 client, add mobile client directories

--- client/audio/audio_qt.h
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: GPLv3-or-later
-// SPDX-FileCopyrightText: Freeciv and Freeciv21 Contributors
-// SPDX-FileCopyrightText: Louis Moureaux <m_louis30@yahoo.com>
-
-#pragma once
-
-void audio_qt_init();
